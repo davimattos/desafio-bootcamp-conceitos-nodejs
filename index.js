5a1eba8b897348d1bc4acf9e2249b8e24eec31e8
davimattos <dmattos340@gmail.com>
2019-06-28 14:52:09 -0300

    implement middlewares and methods (put, delete)

--- index.js
+++ index.js
@@ -5,14 +5,63 @@ app.use(express.json());
 
 let projects = [];
 
-app.post("/projects", (req, res) => {
-  const { id, title, tasks } = req.body;
+app.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Metodo: ${req.method}; URL: ${req.url}`);
+  next();
+  console.timeEnd("Request");
+});
 
-  projects.push({ id, title, tasks });
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(proj => proj.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: " Project not found!" });
+  }
+
+  return next();
+}
+
+app.post("/projects", (req, res) => {
+  const id = Math.floor(Math.random() * 500);
+  const { title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+  projects.push(project);
+
+  return res.json(project);
 });
 
 app.get("/projects", (req, res) => {
   return res.json(projects);
 });
+
+app.put("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(proj => proj.id == id);
+  project.title = title;
+
+  return res.json(project);
+});
+
+app.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  let projIndex = projects.findIndex(proj => proj.id == id);
+  projects.splice(projIndex, 1);
+
+  return res.send();
+});
+
+app.post("/projects/:id/tasks", checkProjectExists, (req, res) => {});
+
+app.post("/projects", (req, res) => {});
 
 app.listen(3000);
